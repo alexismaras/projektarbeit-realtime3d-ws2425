17caaf20a4e3133b1bebb3fa59b677bb64b70269
alexismaras <alexis.maras@live.de>
2025-01-26 19:56:37 +0100

    Surfboard Mesh and Material Switcher Prototype; Scene Environment Updated
    Updates:
    
    Scene Environment:  Added Palmtrees; Changed Camera Transformation and View; Water Setting Adjusted; Lighting Adjusted
    
    Surfbaords: Protoype Script for Changing Shape and Color of the SurfBoards via Keyboard
    
    Performance Benchmark: 190 - 200 FPS

--- projektarbeit_rt3d_produktkonfigurator/Assets/Scripts/SwitchDeck.cs
+++ projektarbeit_rt3d_produktkonfigurator/Assets/Scripts/SwitchDeck.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class SwitchDeck : MonoBehaviour
 {
+    [SerializeField] GameObject surfBoard11;
     [SerializeField] GameObject surfBoard21;
     [SerializeField] GameObject surfBoard22;
     [SerializeField] GameObject surfBoard23;
 
+    [SerializeField] Material greenMaterial;
+    [SerializeField] Material blueMaterial;
+    [SerializeField] Material redMaterial;
+    Material[] faceMaterials;
+
+    MeshRenderer meshRendererSurfBoard11;
     MeshRenderer meshRendererSurfBoard21;
     MeshRenderer meshRendererSurfBoard22;
     MeshRenderer meshRendererSurfBoard23;
+
+
 
     int chosenBoard = 1;
     // Start is called before the first frame update
     void Start()
     {
+        meshRendererSurfBoard11 = surfBoard11.GetComponent<MeshRenderer>();
         meshRendererSurfBoard21 = surfBoard21.GetComponent<MeshRenderer>();
         meshRendererSurfBoard22 = surfBoard22.GetComponent<MeshRenderer>();
         meshRendererSurfBoard23 = surfBoard23.GetComponent<MeshRenderer>();
@@ -36,21 +46,73 @@ public class SwitchDeck : MonoBehaviour
         {
             chosenBoard = 3;
         }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            chosenBoard = 4;
+        }
 
         if (chosenBoard == 1)
         {
+            meshRendererSurfBoard11.enabled = true;
+            meshRendererSurfBoard21.enabled = false;
+            meshRendererSurfBoard22.enabled = false;
+            meshRendererSurfBoard23.enabled = false;
+
+            if (Input.GetKeyDown(KeyCode.Alpha5))
+            {
+                faceMaterials = meshRendererSurfBoard11.materials;
+                faceMaterials[0]= redMaterial;
+                meshRendererSurfBoard11.materials = faceMaterials;
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha6))
+            {
+                faceMaterials = meshRendererSurfBoard11.materials;
+                faceMaterials[0]= greenMaterial;
+                meshRendererSurfBoard11.materials = faceMaterials;
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha7))
+            {
+                faceMaterials = meshRendererSurfBoard11.materials;
+                faceMaterials[0]= blueMaterial;
+                meshRendererSurfBoard11.materials = faceMaterials;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha8))
+            {
+                faceMaterials = meshRendererSurfBoard11.materials;
+                faceMaterials[1]= redMaterial;
+                meshRendererSurfBoard11.materials = faceMaterials;
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha9))
+            {
+                faceMaterials = meshRendererSurfBoard11.materials;
+                faceMaterials[1]= greenMaterial;
+                meshRendererSurfBoard11.materials = faceMaterials;
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha0))
+            {
+                faceMaterials = meshRendererSurfBoard11.materials;
+                faceMaterials[1]= blueMaterial;
+                meshRendererSurfBoard11.materials = faceMaterials;
+            }
+        }
+        else if (chosenBoard == 2)
+        {
+            meshRendererSurfBoard11.enabled = false;
             meshRendererSurfBoard21.enabled = true;
             meshRendererSurfBoard22.enabled = false;
             meshRendererSurfBoard23.enabled = false;
         }
-        else if (chosenBoard == 2)
+        else if (chosenBoard == 3)
         {
+            meshRendererSurfBoard11.enabled = false;
             meshRendererSurfBoard21.enabled = false;
             meshRendererSurfBoard22.enabled = true;
             meshRendererSurfBoard23.enabled = false;
         }
-        else if (chosenBoard == 3)
+        else if (chosenBoard == 4)
         {
+            meshRendererSurfBoard11.enabled = false;
             meshRendererSurfBoard21.enabled = false;
             meshRendererSurfBoard22.enabled = false;
             meshRendererSurfBoard23.enabled = true;
