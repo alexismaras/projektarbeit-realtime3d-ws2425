cadb066f7d801b57065fa1383494aad321828a1c
alexismaras <alexis.maras@live.de>
2025-01-30 03:57:54 +0100

    Surboard Nose-Tail Kombinationen Importiert; MeshSwitch Script generalüberholt, verbessert

--- projektarbeit_rt3d_produktkonfigurator/Assets/Scripts/SwitchDeck.cs
+++ projektarbeit_rt3d_produktkonfigurator/Assets/Scripts/SwitchDeck.cs
@@ -1,31 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 public class SwitchDeck : MonoBehaviour
 {
+    [SerializeField] GameObject surfBoard01;
+    [SerializeField] GameObject surfBoard02;
+    [SerializeField] GameObject surfBoard03;
+    
     [SerializeField] GameObject surfBoard11;
+    [SerializeField] GameObject surfBoard12;
+    [SerializeField] GameObject surfBoard13;
+
     [SerializeField] GameObject surfBoard21;
     [SerializeField] GameObject surfBoard22;
     [SerializeField] GameObject surfBoard23;
 
-    [SerializeField] Material greenMaterial;
-    [SerializeField] Material blueMaterial;
-    [SerializeField] Material redMaterial;
-    Material[] faceMaterials;
+    MeshRenderer meshRendererSurfBoard01;
+    MeshRenderer meshRendererSurfBoard02;
+    MeshRenderer meshRendererSurfBoard03;
 
     MeshRenderer meshRendererSurfBoard11;
+    MeshRenderer meshRendererSurfBoard12;
+    MeshRenderer meshRendererSurfBoard13;
+
     MeshRenderer meshRendererSurfBoard21;
     MeshRenderer meshRendererSurfBoard22;
     MeshRenderer meshRendererSurfBoard23;
 
+    [SerializeField] Material greenMaterial;
+    [SerializeField] Material blueMaterial;
+    [SerializeField] Material redMaterial;
 
 
-    int chosenBoard = 1;
+
+
+    int chosenNose= 0;
+    int chosenTail= 1;
     // Start is called before the first frame update
     void Start()
     {
+        meshRendererSurfBoard01 = surfBoard01.GetComponent<MeshRenderer>();
+        meshRendererSurfBoard02 = surfBoard02.GetComponent<MeshRenderer>();
+        meshRendererSurfBoard03 = surfBoard03.GetComponent<MeshRenderer>();
+
         meshRendererSurfBoard11 = surfBoard11.GetComponent<MeshRenderer>();
+        meshRendererSurfBoard12 = surfBoard12.GetComponent<MeshRenderer>();
+        meshRendererSurfBoard13 = surfBoard13.GetComponent<MeshRenderer>();
+
         meshRendererSurfBoard21 = surfBoard21.GetComponent<MeshRenderer>();
         meshRendererSurfBoard22 = surfBoard22.GetComponent<MeshRenderer>();
         meshRendererSurfBoard23 = surfBoard23.GetComponent<MeshRenderer>();
@@ -36,87 +59,101 @@ public class SwitchDeck : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            chosenBoard = 1;
+            chosenNose = 0;
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            chosenBoard = 2;
+            chosenNose = 1;
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            chosenBoard = 3;
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            chosenBoard = 4;
+            chosenNose = 2;
         }
 
-        if (chosenBoard == 1)
+        if (Input.GetKeyDown(KeyCode.Q))
         {
-            meshRendererSurfBoard11.enabled = true;
-            meshRendererSurfBoard21.enabled = false;
-            meshRendererSurfBoard22.enabled = false;
-            meshRendererSurfBoard23.enabled = false;
+            chosenTail = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            chosenTail = 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            chosenTail = 3;
+        }
 
-            if (Input.GetKeyDown(KeyCode.Alpha5))
-            {
-                faceMaterials = meshRendererSurfBoard11.materials;
-                faceMaterials[0]= greenMaterial;
-                meshRendererSurfBoard11.materials = faceMaterials;
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha6))
-            {
-                faceMaterials = meshRendererSurfBoard11.materials;
-                faceMaterials[0]= blueMaterial;
-                meshRendererSurfBoard11.materials = faceMaterials;
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha7))
-            {
-                faceMaterials = meshRendererSurfBoard11.materials;
-                faceMaterials[0]= blueMaterial;
-                meshRendererSurfBoard11.materials = faceMaterials;
-            }
+        ChooseBoard();
 
-            if (Input.GetKeyDown(KeyCode.Alpha8))
-            {
-                faceMaterials = meshRendererSurfBoard11.materials;
-                faceMaterials[1]= redMaterial;
-                meshRendererSurfBoard11.materials = faceMaterials;
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha9))
-            {
-                faceMaterials = meshRendererSurfBoard11.materials;
-                faceMaterials[1]= greenMaterial;
-                meshRendererSurfBoard11.materials = faceMaterials;
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha0))
-            {
-                faceMaterials = meshRendererSurfBoard11.materials;
-                faceMaterials[1]= blueMaterial;
-                meshRendererSurfBoard11.materials = faceMaterials;
-            }
-        }
-        else if (chosenBoard == 2)
+
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            meshRendererSurfBoard11.enabled = false;
-            meshRendererSurfBoard21.enabled = true;
-            meshRendererSurfBoard22.enabled = false;
-            meshRendererSurfBoard23.enabled = false;
+            AllMaterialSet(redMaterial);
         }
-        else if (chosenBoard == 3)
+        else if (Input.GetKeyDown(KeyCode.S))
         {
-            meshRendererSurfBoard11.enabled = false;
-            meshRendererSurfBoard21.enabled = false;
-            meshRendererSurfBoard22.enabled = true;
-            meshRendererSurfBoard23.enabled = false;
+            AllMaterialSet(blueMaterial);
         }
-        else if (chosenBoard == 4)
+        else if (Input.GetKeyDown(KeyCode.D))
         {
-            meshRendererSurfBoard11.enabled = false;
-            meshRendererSurfBoard21.enabled = false;
-            meshRendererSurfBoard22.enabled = false;
-            meshRendererSurfBoard23.enabled = true;
+            AllMaterialSet(greenMaterial);
         }
+
         
+        
+    }
+
+    void ChooseBoard()
+    {
+        for (var n = 0; n < 3; n++)
+        {
+            for (var t = 1; t < 4; t++)
+            {
+                MeshRenderer boardMeshRenderer =  GetVariableContaining($"meshRendererSurfBoard{n}{t}");
+                if (n == chosenNose && t == chosenTail)
+                {
+                    boardMeshRenderer.enabled = true;
+                }
+                else
+                {
+                    boardMeshRenderer.enabled = false;
+                }
+            }
+        }
+    }
+
+    void AllMaterialSet(Material boardMaterial)
+    {
+        for (var n = 0; n < 3; n++)
+        {
+            for (var t = 1; t < 4; t++)
+            {
+                MaterialGetSet(GetVariableContaining($"meshRendererSurfBoard{n}{t}"), boardMaterial);
+            }
+        }
+    }
+
+    void MaterialGetSet(MeshRenderer boardMeshRenderer, Material boardMaterial)
+    {
+        Material[] faceMaterials = boardMeshRenderer.materials;
+        faceMaterials[0]= boardMaterial;
+        boardMeshRenderer.materials = faceMaterials;
+    }
+
+    MeshRenderer GetVariableContaining(string searchString)
+    {
+        // Get all fields in this class
+        FieldInfo[] fields = this.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+
+        foreach (FieldInfo field in fields)
+        {
+            // Check if the field name contains the search string
+            if (field.Name.Contains(searchString))
+            {
+                return field.GetValue(this) as MeshRenderer;
+            }
+        }
+
+        return null; // Return null if no match is found
     }
 }
