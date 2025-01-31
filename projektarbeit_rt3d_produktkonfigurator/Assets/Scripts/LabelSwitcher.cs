7e1d05268f2b2f22b991f495b07a36e2fc9f47fe
alexismaras <alexis.maras@live.de>
2025-01-31 16:50:25 +0100

    Converted to WebGL

---
+++ projektarbeit_rt3d_produktkonfigurator/Assets/Scripts/LabelSwitcher.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LabelSwitcher : MonoBehaviour
+{
+    [SerializeField] SwitchDeck switchDeck;
+
+    [SerializeField] TMP_Text finLabel;
+    [SerializeField] TMP_Text noseLabel;
+    [SerializeField] TMP_Text tailLabel;
+
+    [SerializeField] string nose0 = "POINTED";
+    [SerializeField] string nose1 = "POINTED ROUND";
+    [SerializeField] string nose2 = "ROUND";
+
+    [SerializeField] string tail1 = "SQUARE";
+    [SerializeField] string tail2 = "ROUNDED";
+    [SerializeField] string tail3 = "SWALLOW";
+
+    [SerializeField] string fin1 = "RAKE";
+    [SerializeField] string fin2 = "HATCHET";
+    [SerializeField] string fin3 = "D-FIN";
+
+    int currentChosenNose;
+    int currentChosenTail;
+
+    int currentChosenFin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        noseLabel.text = GetStringContaining($"nose{switchDeck.chosenNose}");
+        tailLabel.text = GetStringContaining($"tail{switchDeck.chosenTail}");
+        finLabel.text = GetStringContaining($"fin{switchDeck.chosenFin}");
+        currentChosenNose = switchDeck.chosenNose;
+        currentChosenTail = switchDeck.chosenTail;
+        currentChosenFin = switchDeck.chosenFin;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (switchDeck.chosenNose != currentChosenNose)
+        {
+            noseLabel.text = GetStringContaining($"nose{switchDeck.chosenNose}");
+            currentChosenNose = switchDeck.chosenNose;
+        }
+        if (switchDeck.chosenTail != currentChosenTail)
+        {
+            tailLabel.text = GetStringContaining($"tail{switchDeck.chosenTail}");
+            currentChosenTail = switchDeck.chosenTail;
+        }
+        if (switchDeck.chosenFin != currentChosenFin)
+        {
+            finLabel.text = GetStringContaining($"fin{switchDeck.chosenFin}");
+            currentChosenFin = switchDeck.chosenFin;
+        }
+    }
+
+    string GetStringContaining(string searchString)
+    {
+        // Get all fields in this class
+        FieldInfo[] fields = this.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+
+        foreach (FieldInfo field in fields)
+        {
+            // Check if the field name contains the search string
+            if (field.Name.Contains(searchString))
+            {
+                return field.GetValue(this) as string;
+            }
+        }
+
+        return null; // Return null if no match is found
+    }
+}
